64449baa98a37df4ab3a6c69865c645def429a8e
Brecht Devleesschauwer <Brecht.Devleesschauwer@UGent.be>
2016-02-07 18:48:04 -0500

    introduce doc() function

--- preamble.R
+++ preamble.R
@@ -1,5 +1,7 @@
 ### Preamble for Rmarkdown files
 
+### Settings
+
 ## attach prevalence package
 library(prevalence)
 pkg <- packageVersion("prevalence")
@@ -9,8 +11,16 @@ knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>")
 
+### Utility functions
+
 ## format double as percentage
 percent <-
   function(x, dig = 0) {
     paste0(round(100*x, dig), "%")
+  }
+
+## create documentation link
+doc <-
+  function(x) {
+    sprintf("[`%1$s`](?main=docs&sub=%1$s)", x)
   }
